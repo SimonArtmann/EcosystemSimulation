e66a790b86c6a2953f34f66e5737249b0be110f7
Simon Artmann <simonart0605@gmail.com>
2022-03-17 17:03:46 +0100

    basics in form of comments

---
+++ main.py
@@ -0,0 +1,9 @@
+# 2 species: neutral; hostile
+# neutral: 1 apple to survive, 3 to replicate
+# hostile: 1 apple to survive, 3 kills to replicate
+# 2 hostile on 1 tree = both dead
+# 2 passive on 1 tree = share
+# hostile attacks -> loses 10 apples
+# every tree drops 1-7 apples; set number of trees
+# set number of days running, after every day number of survivors is displayed
+# (predators); if attacked by hostile, 99% chance to survive, needs 1 per 10 gens to #replicate
