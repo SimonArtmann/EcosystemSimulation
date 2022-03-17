07a5601b19ada48c15aef19af3226ff37fb2b09c
Simon Artmann <simonart0605@gmail.com>
2022-03-17 17:45:46 +0100

    added list

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 # every tree drops 1-7 apples; set number of trees
 # set number of days running, after every day number of survivors is displayed
 # (predators); if attacked by hostile, 99% chance to survive, needs 1 per 10 gens #to replicate
+import random
+
+
 class Char:
     def __init__(self, foodSupply):
         self.foodSupply = foodSupply
@@ -30,5 +33,68 @@ class Tree:
     def __init__(self, randFood):
         self.randFood = randFood
 
+    def genRandom():
+        rand = random.randint(1, 7)
+        randFood = rand
+        print(randFood)
 
-# did
+
+def genRandom():
+    Tree.genRandom()
+
+
+listOfTrees = (
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    48,
+    49,
+    50,
+)
+
+if __name__ == "__main__":
+    genRandom()
