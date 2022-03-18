503b50a0ed2664a3fe92a90d8831de2d2012f548
Simon Artmann <simonart0605@gmail.com>
2022-03-18 14:18:42 +0100

    Infostunde 18.3 added

--- main.py
+++ main.py
@@ -10,6 +10,7 @@
 #Ausgabewerte von genRandom in Liste speichern, wenn gleiche Zahl nacheinander -> #Attack
 import random
 
+
 class Neutral:
     def __init__(self, foodSupply, lifeCount):
         self.foodSupply = foodSupply
@@ -23,6 +24,17 @@ class Neutral:
             n.foodSupply += t.food 
             j -= 1
             print (n.foodSupply)
+    
+    def surviveAndReplicate(self):
+        cache = n.foodSupply - n.lifeCount
+        if cache < 0:
+            n.foodSupply + n.lifeCount
+            n.foodSupply = 0
+        else:
+            replicationNeed = n.foodSupply / 5
+            n.lifeCount += replicationNeed
+
+
 
 class Hostile:
     def __init__(self, foodSupply, killCount, lifeCount):
@@ -38,18 +50,18 @@ class Tree:
         rand = random.randint(1, 7)
         randFood = rand
         self.food = randFood
-        print (randFood)
+    
 
     def randomTree(self):
         rand2 = random.randint(0, 50)
         n = rand2
         treeID = listOfTrees[n - 1]
-        print(treeID)
+      
 
 listOfTrees = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50)
 
-def genRandom():
-    t.genRandom()
+def genRandomFood():
+    t.genRandomFood()
 
 def randomTree():
     t.randomTree()
@@ -57,8 +69,19 @@ def randomTree():
 def moveToTree():
     n.moveToTree()
 
+def surviveAndReplicate():
+    n.surviveAndReplicate()
+
 if __name__ == "__main__":
+    runningDays = int(input ("How many days do you want the simulation to run? "))
     n = Neutral(0, 1)
     h = Hostile(0, 0, 0)
     t = Tree(0)
-    moveToTree()
+    while runningDays > 0:
+        t.randomTree()
+        t.genRandomFood()
+        n.moveToTree()
+        n.surviveAndReplicate()
+        print("There are currently " + str(n.lifeCount) + " neutrals alive. (Day " + str(runningDays) + ")")
+        runningDays -= 1
+        
