23d43a8175e7a0d4860d50ed9be416b8cc251ffa
Simon Artmann <simonart0605@gmail.com>
2022-03-19 22:08:31 +0100

    hostiles deleted, saving results added

--- main.py
+++ main.py
@@ -34,29 +34,6 @@ class Neutral:
             n.lifeCount += replicationNeed
             diffBtwFood = 0
 
-class Hostile:
-    def __init__(self, foodSupply, killCount, lifeCount):
-        self.foodSupply = foodSupply
-        self.killCount = killCount
-        self.lifeCount = lifeCount
-        
-    def moveToTreeH(self):
-        i = n.lifeCount
-        while i > 0:
-            rand = random.randint(0, 49)
-            h.foodSupply += t.listOfTreesAndFood[rand]
-            t.listOfTreesAndFood[rand] = 0
-            i -= 1
-
-    def surviveAndReplicateH(self):
-        cache = h.foodSupply - h.lifeCount
-        if cache < 0:
-            h.foodSupply + h.lifeCount
-            h.foodSupply = 0
-        else:
-            replicationNeed = h.foodSupply / 5
-            h.lifeCount += replicationNeed
-
 class Tree:
     def __init__(self, listOfTreesAndFood):
         self.listOfTreesAndFood = listOfTreesAndFood
@@ -77,37 +54,29 @@ def moveToTreeN():
 def surviveAndReplicateN():
     n.surviveAndReplicateN()
 
-def moveToTreeH():
-    h.moveToTreeH()
-
-def surviveAndReplicateH():
-    h.surviveAndReplicateH()
-
 def setTreesBack():
     t.setTreesBack()
 
 if __name__ == "__main__":
     runningDays = int(input ("How many days do you want the simulation to run? "))
     n = Neutral(0, 1)
-    h = Hostile(0, 0, 1)
     t = Tree([])
     i = 1
     while i <= runningDays:   
-        t.genTreesAndFood()
-        #h.moveToTreeH()
-        #h.surviveAndReplicateH()   
+        t.genTreesAndFood()   
         n.moveToTreeN()
         n.surviveAndReplicateN()
         if n.lifeCount == 1:
             print("There is currently " + str(int (n.lifeCount)) + " neutral alive. (Day " + str(i) + ")")
+            resultsFile = open("resultsFile.txt", "a")
+            resultsFile.write(str(int(n.lifeCount)) + "\n")
+            resultsFile.close()
         else:
             print("There are currently " + str(int (n.lifeCount)) + " neutrals alive. (Day " + str(i) + ")")
+            resultsFile = open("resultsFile.txt", "a")
+            resultsFile.write(str(int(n.lifeCount)) + "\n")
+            resultsFile.close()
         
-        #if h.lifeCount == 1:
-            #print("There is currently " + str(int (h.lifeCount)) + " hostile alive. (Day " + str(i) + ")")
-        #else:
-            #print("There are currently " + str(int (h.lifeCount)) + " hostiles alive. (Day " + str(i) + ")")
         i += 1
-        print (t.listOfTreesAndFood)
         t.listOfTreesAndFood = []
-        n.foodSupply = 0    
+        n.foodSupply = 0   
